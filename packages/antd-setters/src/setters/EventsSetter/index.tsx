9e7d5281d1382a4e4a1cd586695fb2a259329d22
ruhuo.tan <ruhuo.tan@shopee.com>
2022-06-07 15:06:27 +0800

    refactor: move types and consts

--- packages/antd-setters/src/setters/EventsSetter/index.tsx
+++ packages/antd-setters/src/setters/EventsSetter/index.tsx
@@ -1,144 +1,17 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import {
-  Button,
-  Collapse,
-  Divider,
-  Empty,
-  Modal,
-  Tooltip,
-  Popconfirm
-} from 'antd'
+import { Button, Collapse, Divider, Empty, Modal, Popconfirm } from 'antd'
 import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons'
-import { ISchema } from '@formily/react'
 import { isArr, isStr } from '@formily/shared'
 import { createForm } from '@formily/core'
 import { Form } from '@formily/antd'
 import { cloneDeep } from 'lodash'
 import { SchemaField } from './components/SchemaField'
 import { Header } from './components/Header'
+import { ActionType, IAction, IEventsSetterProps } from './types'
+import { builtInActions, customActions } from './const'
 import './index.less'
 
 const { Panel } = Collapse
-
-interface IEventItem {
-  name: string
-  template?: string
-}
-
-interface IDefinitionItem {
-  type?: string
-  title?: string
-  list?: IEventItem[]
-}
-
-interface IValueItem {
-  eventName: string
-  actions: IAction[]
-}
-
-export interface IEventsSetterProps {
-  definition: IDefinitionItem[]
-  value?: IValueItem[]
-  onChange?: (value?: IValueItem[]) => void
-}
-
-export enum ActionType {
-  BuiltIn = 'builtIn',
-  Custom = 'custom'
-}
-
-export interface IAction {
-  type: ActionType
-  name?: string
-  desc?: string
-  params?: ISchema
-  value?: any
-}
-
-// 内置动作，点击内置动作选项后渲染参数表单，表单使用 formily 渲染
-const builtInActions: IAction[] = [
-  {
-    type: ActionType.BuiltIn,
-    name: 'openPage',
-    desc: '打开页面',
-    params: {
-      type: 'object',
-      properties: {
-        url: {
-          name: 'url',
-          type: 'string',
-          title: 'url',
-          'x-decorator': 'FormItem',
-          'x-component': 'Input',
-          required: true
-        },
-        isBlank: {
-          name: 'isBlank',
-          type: 'boolean',
-          title: '在新页面打开',
-          'x-decorator': 'FormItem',
-          'x-component': 'Switch'
-        }
-      }
-    }
-  },
-  {
-    type: ActionType.BuiltIn,
-    name: 'message',
-    desc: '消息提示',
-    params: {
-      type: 'object',
-      properties: {
-        message: {
-          name: 'message',
-          type: 'string',
-          title: '消息内容',
-          'x-decorator': 'FormItem',
-          'x-component': 'Input',
-          required: true
-        }
-      }
-    }
-  },
-  {
-    type: ActionType.BuiltIn,
-    name: 'goBack',
-    desc: '返回上一页'
-  },
-  {
-    type: ActionType.BuiltIn,
-    name: 'openModal ',
-    desc: '打开弹窗'
-  }
-]
-
-const customActions: IAction[] = [
-  {
-    type: ActionType.Custom,
-    name: 'custom',
-    desc: '自定义代码',
-    params: {
-      type: 'object',
-      properties: {
-        name: {
-          name: 'name',
-          type: 'string',
-          title: '动作名称',
-          'x-decorator': 'FormItem',
-          'x-component': 'Input',
-          required: true
-        },
-        isSetExtendParams: {
-          name: 'isSetExtendParams',
-          type: 'boolean',
-          title: '扩展参数设置',
-          'x-decorator': 'FormItem',
-          'x-component': 'Switch'
-        }
-      }
-    }
-  }
-]
 
 /**
  * 事件属性配置组件，支持在一个事件中配置多个动作
