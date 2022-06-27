97c00a743b42519bdc166190f69ccfce345602a5
ruhuo.tan <ruhuo.tan@shopee.com>
2022-06-27 14:40:23 +0800

    chore: remove useless file

--- packages/antd-materials/script.ts
+++
@@ -1,9 +0,0 @@
-import { uploadCdn } from '../../scripts/cdn'
-import pkg from './package.json'
-
-const pckName = `${pkg.name.replace('@seada/', '')}@${pkg.version}`
-
-uploadCdn('./build', pckName).then((cdnUrls) => {
-  // eslint-disable-next-line no-console
-  console.log(cdnUrls)
-})
