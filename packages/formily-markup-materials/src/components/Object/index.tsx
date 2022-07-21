7c28bc983d8800e045b53a0709e735e06cd27715
bingblog <abingblog@gmail.com>
2022-07-21 11:18:13 +0800

    feat: add useUpdateForm when field props changed; receive props from config;

--- packages/formily-markup-materials/src/components/Object/index.tsx
+++ packages/formily-markup-materials/src/components/Object/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createElement } from 'react';
 import { FormLayout } from '@formily/antd';
-import { useSchemaField, useHasPlaceholder, useInDesignMode } from '../../shared/hooks';
+import {
+  useSchemaField,
+  useHasPlaceholder,
+  useInDesignMode,
+  useComponentProps,
+  useUpdateForm,
+} from '../../shared/hooks';
 
 const FormilyObject: React.ForwardRefRenderFunction<any, any> = React.forwardRef((props, ref) => {
   const SchemaField = useSchemaField();
-
   const hasPlaceholder = useHasPlaceholder(props);
 
-  const inDesign = useInDesignMode(props);
+  const { fieldProps, children } = props;
+
+  useUpdateForm([props]);
 
   console.log('FormilyObject', props);
 
@@ -17,25 +25,16 @@ const FormilyObject: React.ForwardRefRenderFunction<any, any> = React.forwardRef
       <SchemaField.Void
         x-component="Slot"
         x-component-props={{
-          children: <div ref={ref}>{props.children}</div>,
+          children: <div ref={ref}>{children}</div>,
         }}
       />
     );
   }
 
-  console.log(inDesign, 'inDesign');
-  // if (inDesign) {
-  //   return <SchemaField.Void
-  //     x-component="Slot"
-  //     x-component-props={{
-  //       children: <div ref={ref}>{ props.children }</div>
-  //     }}
-  //   />
-  // }
-
   console.log('FormilyObject FormLayout props', props);
   return (
     <SchemaField.Void
+      {...fieldProps}
       x-component={(props) => {
         // @ts-ignore
         return (
@@ -44,6 +43,7 @@ const FormilyObject: React.ForwardRefRenderFunction<any, any> = React.forwardRef
           </div>
         );
       }}
+      x-component-props={useComponentProps(props)}
     >
       <SchemaField.Object name="person">{props.children}</SchemaField.Object>
     </SchemaField.Void>
